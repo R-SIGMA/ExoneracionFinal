1a3e85cc06c97beba7116bf2d1ed3393bff65c85
Andrea Sotil <andreasotil@ieholding.com>
2022-08-11 21:00:03 -0500

    commit

--- webserv/src/main/java/com/idat/webserv/service/ProfesorService.java
+++ webserv/src/main/java/com/idat/webserv/service/ProfesorService.java
@@ -1,5 +1,16 @@
 package com.idat.webserv.service;
 
+import java.util.List;
+
+import com.idat.webserv.model.Profesor;
+
 public interface ProfesorService {
 
+	public void guardarProfesor(Profesor profesor);
+	public void editarProfesor(Profesor profesor);
+	
+	public void eliminarProfesor(Integer id);
+	
+	public List<Profesor> listarProfesor();
+	public Profesor obtenerProfesorId(Integer id);
 }
--- webserv/src/main/java/com/idat/webserv/service/ProfesorServiceImpl.java
+++ webserv/src/main/java/com/idat/webserv/service/ProfesorServiceImpl.java
@@ -1,5 +1,51 @@
 package com.idat.webserv.service;
 
-public class ProfesorServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.idat.webserv.model.Profesor;
+import com.idat.webserv.repository.ProfesorRepository;
+
+public class ProfesorServiceImpl implements ProfesorService
+{
+	@Autowired
+	public ProfesorRepository rep;
+
+	@Override
+	public void guardarProfesor(Profesor profesor) {
+		Profesor p = new Profesor();
+		p.setIdProfesor(profesor.getIdProfesor());
+		p.setNombre(profesor.getNombre());
+		p.setApellido(profesor.getApellido());
+		p.setFechaContrato(profesor.getFechaContrato());
+		p.setEspecialidad(profesor.setFechaContrato(p));
+		rep.save(p);
+		
+	}
+
+	@Override
+	public void editarProfesor(Profesor profesor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarProfesor(Integer id) {
+		rep.deleteById(id);
+		
+	}
+
+	@Override
+	public List<Profesor> listarProfesor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Profesor obtenerProfesorId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
